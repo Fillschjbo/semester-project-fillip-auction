07c1a0fee44ae85447f72e4dbbb696a439a6f960
fillip husebø <felipehusebo@gmail.com>
2024-11-14 17:14:46 +0100

    added folder structure and routing using vite

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -3,6 +3,24 @@ export default async function router(pathname = window.location.pathname) {
         case "/":
             await import("./views/home.js");
             break;
+        case "/auth/login/":
+            await import("./views/login.js");
+            break;
+        case "/auth/register/":
+            await import("./views/register.js");
+            break;
+        case "/listing/":
+            await import("./views/listing.js");
+            break;
+        case "/listing/create/":
+            await import("./views/listingCreate.js");
+            break;
+        case "/listing/edit/":
+            await import("./views/listingCreate.js");
+            break;
+        case "/profile/":
+            await import("./views/profile.js");
+            break;
 
         default:
             await import("./views/notFound.js");
