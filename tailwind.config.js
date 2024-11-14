05460642a8c911d589120c177d81c9121b318d01
fillip husebø <felipehusebo@gmail.com>
2024-11-14 15:28:48 +0100

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
