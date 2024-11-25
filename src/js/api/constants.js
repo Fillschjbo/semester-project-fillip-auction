7f43299c74013be61d0e20308de8f820d31bb15f
fillip husebø <felipehusebo@gmail.com>
2024-11-25 15:19:05 +0100

    CONSTANTS: Set up API URL paths

---
+++ src/js/api/constants.js
@@ -0,0 +1,15 @@
+export const API_KEY = "7c5e4be3-e45e-4b27-98b8-076dd2b13f1a";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUCTION = `${API_BASE}/auction`;
+
+export const API_AUCTION_LISTINGS = `${API_AUCTION}/listings`;
+
+export const API_AUCTION_PROFILES = `${API_AUCTION}/profiles`;
